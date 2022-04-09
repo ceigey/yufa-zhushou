ff11873e382bf2972833944cff1420633257754a
Christian Meredith <ceigey@users.noreply.github.com>
2022-04-09 21:30:36 +0930

    Messing with fonts and assets and layouts!

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const inc = () => count.value++;
+const dec = () => count.value--;
+</script>
+
+<template>
+  <div>
+    <button @click="dec(count)">to {{ count - 1 }}</button>
+    <button @click="inc(count)">to {{ count + 1 }}</button>
+    <p>Current count: {{ count }}</p>
+  </div>
+</template>
+
+<style scoped>
+button {
+  min-width: 5em;
+}
+</style>
---
+++ src/components/Ruby.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps } from "vue";
+
+const { text } = defineProps({
+    text: String,
+});
+</script>
+
+<template>
+    <ruby class="kaiti">
+        <slot />
+        <rp>(</rp>
+        <rt class="roman">{{ text }}</rt>
+        <rp>)</rp>
+    </ruby>
+</template>
+
+<style scoped>
+.kaiti {
+    font-family: "Ma Shan Zheng";
+}
+.roman {
+    /* font-family: "Merriweather"; */
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+        Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    font-weight: normal;
+}
+</style>
